a2b74ec5368ef98b400f23e5c7249847c318670a
pupixipup <rkbitinas@gmail.com>
2022-09-12 19:56:34 +0200

    feat: add list of visited places

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -4,6 +4,13 @@ import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 type MapProp = google.maps.Map | null;
 type CenterCoordinates = google.maps.LatLngLiteral | google.maps.LatLng | undefined;
+interface ILocationResponse extends Response {
+  plus_code: {
+    compound_code: string,
+    global_code: string
+  }
+  global_code: string
+}
 
 const initialLocation = { lat: 57.7412801, lng: 14.131976 };
 
@@ -16,10 +23,23 @@ function Map() {
   const [manualCenter, setManualCenter] = useState<CenterCoordinates>(initialLocation);
   const [center, setCenter] = useState<google.maps.LatLngLiteral>(initialLocation);
   const [mapRef, setMapRef] = useState<MapProp>(null);
+  const [visitedPlaces, setVisitedPlaces] = useState<string[]>([]);
   const { isLoaded } = useLoadScript( {
-    googleMapsApiKey: ""
+    googleMapsApiKey: "AIzaSyANzx1HF-2J9wd5FBeNY4yv0PQfBhJraIQ"
     // Insert API key here
   });
+
+  function updatePlaces(newCenter: google.maps.LatLngLiteral) {
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${newCenter.lat},${newCenter.lng}&key=AIzaSyANzx1HF-2J9wd5FBeNY4yv0PQfBhJraIQ`)
+        .then((data) =>  data.json())
+        .then((json: ILocationResponse) => {
+          const newPlace = json.plus_code.compound_code || json.plus_code.global_code;
+          console.log(json);
+          if (!visitedPlaces.includes(newPlace)) {
+            setVisitedPlaces([...visitedPlaces, newPlace]);
+          }
+        });
+  }
 
   const handleCenterChanged = () => {
     if (mapRef) {       
@@ -44,8 +64,13 @@ function Map() {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   }, []);
 
+  useEffect(() => {
+  updatePlaces(manualCenter as google.maps.LatLngLiteral);
+  }, [manualCenter]);
+
   return isLoaded ? (
     <div className='map'>
+      <div className="map__wrapper">
       <h3 className='map__title'>Map project</h3>
       <GoogleMap
       zoom={zoom}
@@ -70,6 +95,15 @@ function Map() {
       <h4 className="map__position">
         Latitude: {center.lat.toFixed(2)}, Longtitude: {center.lng.toFixed(2)}
       </h4>
+      </div>
+      <div className="map__places">
+        
+      {visitedPlaces.map((place, index) => {
+        return(
+          <div className='map__places-item' key={`${place}-${index}`}>{place}</div>
+          )
+        })}
+        </div>
     </div>
   ) : <div>Error occured. Please, try to reload the page</div>
 }
