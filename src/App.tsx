4713aaf982ae93ff90c72a79089b50ce3cf4a2fa
pupixipup <rkbitinas@gmail.com>
2022-09-12 16:52:25 +0200

    feat: rebuild App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import Map from './Map/Map';
 import './App.css';
 
 function App() {
+
   return (
     <div className="wrapper">
       <Map/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
